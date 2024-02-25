61fdeca88c7cadc8c92529066abe6ecfa6681419
deanone <athanasios.salamanis@gmail.com>
2024-02-25 14:52:42 +0200

    Fixed serial implementation (minor fixes).

--- dot-product.c
+++ dot-product.c
@@ -2,35 +2,43 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-  clock_t startTime = clock();
-  int N = atoi(argv[1]);
-  
-  /* vector 1 */
-  double* vec1;
-  vec1 = malloc(N * sizeof(double));
-  for (int i = 0; i < N; i++)
-  {
-    vec1[i] = i;
-  }
 
-  /* vector 2 */
-  double* vec2;
-  vec2 = malloc(N * sizeof(double));
-  for (int i = N - 1; i >= 0; i--)
+  if (argc > 1)
   {
-    vec2[(N - 1) - i] = i;
-  }
+    clock_t startTime = clock();
+    int N = atoi(argv[1]);
+    
+    // vector 1 
+    double* vec1;
+    vec1 = malloc(N * sizeof(double));
+    for (int i = 0; i < N; i++)
+    {
+      vec1[i] = i;
+    }
 
-  double finalResult = 0.0;
-  for (int i = 0; i < N; i++)
+    // vector 2
+    double* vec2;
+    vec2 = malloc(N * sizeof(double));
+    for (int i = N - 1; i >= 0; i--)
+    {
+      vec2[(N - 1) - i] = i;
+    }
+
+    double finalResult = 0.0;
+    for (int i = 0; i < N; i++)
+    {
+      finalResult += vec1[i] * vec2[i];
+    }
+    
+    double elapsedTime = (double)(clock() - startTime) / CLOCKS_PER_SEC;
+
+    printf("\nThe dot product is: %f\n", finalResult);
+    printf( "Total time spent in seconds: %f\n\n", elapsedTime);
+  }
+  else
   {
-    finalResult += vec1[i] * vec2[i];
+    printf("\nPlease provide the size of input vectors as command line argument, i.e., ./doc-product.out N\n\n");
   }
-  
-  double elapsedTime = (double)(clock() - startTime) / CLOCKS_PER_SEC;
-
-  printf("\nThe dot product is: %f\n", finalResult);
-  printf( "Total time spent in seconds: %f\n", elapsedTime);
 }
